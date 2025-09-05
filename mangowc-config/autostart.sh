f2bfc16380f8113a5851a805da0445a5206899ff
Lars <bumbleberrycar@gmail.com>
2025-09-05 22:15:31 +0200

    chore: add wallpaper and notification startup

--- mangowc-config/autostart.sh
+++ mangowc-config/autostart.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
-# Start Waybar with the provided configuration
+
+# Update environment for Wayland
 dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
+
+# Set wallpaper
+swaybg -i ~/.config/mango/wallpapers/default.jpg &
+
+# Launch notification daemon
+mako &
+
+# Start Waybar with the provided configuration
 waybar -c ~/.config/mango/waybar/config -s ~/.config/mango/waybar/style.css &
